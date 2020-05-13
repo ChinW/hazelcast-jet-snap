ac246ec69785c8fff88606aa5e2fd3cc8bbd6434
Chi <contractswif@hotmail.com>
2020-05-13 11:16:49 +0800

    4.1

---
+++ bin/common.sh
@@ -0,0 +1,30 @@
+SCRIPT_DIR="$(dirname "$0")"
+JET_HOME="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+if [ "$JAVA_HOME" ]; then
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA="$(which java 2>/dev/null)"
+fi
+
+if [ -z "$JAVA" ]; then
+    echo "Cannot find a way to start the JVM: neither JAVA_HOME is set nor the java command is on the PATH"
+    exit 1
+fi
+
+# 1 -> Java 8 or earlier (1.8..)
+# 9, 10, 11 -> JDK9, JDK10, JDK11 etc.
+JAVA_VERSION=$(${JAVA} -version 2>&1 | sed -En 's/.* version "([0-9]+).*$/\1/p')
+if [ "$JAVA_VERSION" -ge "9" ]; then
+    JDK_OPTS="\
+        --add-modules java.se \
+        --add-exports java.base/jdk.internal.ref=ALL-UNNAMED \
+        --add-opens java.base/java.lang=ALL-UNNAMED \
+        --add-opens java.base/java.nio=ALL-UNNAMED \
+        --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+        --add-opens java.management/sun.management=ALL-UNNAMED \
+        --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
+    "
+fi
+
+CLASSPATH="$JET_HOME/lib/*:$CLASSPATH"
